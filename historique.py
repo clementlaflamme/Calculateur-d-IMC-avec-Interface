6081ae371a578271d8cc665e0087168358c8c1ff
Clement Laflamme <215926899+clementlaflamme@users.noreply.github.com>
2025-12-09 16:29:45 -0500

    Ajout des fonctions de calcul

---
+++ historique.py
@@ -0,0 +1,8 @@
+def demander_infos():
+
+# Demande poids et taille
+
+
+def afficher_resultat(imc, categorie):
+
+# Affiche joliment
---
+++ interface.py
@@ -0,0 +1,7 @@
+def sauvegarder_calcul(nom, imc):
+
+# Sauvegarde dans un fichier
+
+def afficher_historique():
+
+# Lit le fichier
