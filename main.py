270cd273b50ac6783e40109f7ae6ee583180865a
Marc-André Dufour <MAD@nottrue.test>
2025-12-15 20:51:34 -0500

    Fix:mis en page

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import calculs
 import historique
 import interface
 
-#*** MAIN ***
+#   *** MAIN ***
 
 # Gestion de l'interface
-
 nom, poids, taille = interface.demander_infos()
 
-# Gestion des calculs
-# poids en KG et taille en m(1.68)
-
+# Gestion des calculs ** poids en KG et taille en metre(1.68)
 imc = calculs.calculer_imc(poids, taille)
 stat = calculs.interpreter_imc(imc)
 
+# Afficher Résultat
 interface.afficher_resultat(nom, imc, stat)
 
 # Gestion de la sauvegarde
-
 historique.sauvegarder_calcul(nom, imc)
-
 historique.afficher_historique(f"{nom}.txt")
